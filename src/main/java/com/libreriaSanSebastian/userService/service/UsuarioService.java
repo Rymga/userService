e6c2fdce07954a7118acbe8a0f7a2289c55335a0
fr <fr.choque@duocuc.cl>
2025-05-30 04:45:28 -0400

    service v2

--- src/main/java/com/libreriaSanSebastian/userService/service/UsuarioService.java
+++ src/main/java/com/libreriaSanSebastian/userService/service/UsuarioService.java
@@ -1,4 +1,40 @@
 package com.libreriaSanSebastian.userService.service;
 
+import com.libreriaSanSebastian.userService.model.Usuario;
+import com.libreriaSanSebastian.userService.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Optional<Usuario> buscarPorNombre(String nombre) {
+        return Optional.ofNullable(usuarioRepository.findByNombre(nombre));
+    }
+
+    public Optional<Usuario> buscarPorRut(String rut) {
+        return Optional.ofNullable(usuarioRepository.findByRut(rut));
+    }
+
+    public Usuario guardar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void eliminar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
 }
